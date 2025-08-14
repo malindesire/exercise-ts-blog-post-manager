45ff197b356ea0097fcd6377c81c54d5a5c880b8
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-14 16:18:20 +0200

    Move form functionality to seperate file

--- bloggporten/src/main.ts
+++ bloggporten/src/main.ts
@@ -1,38 +1,6 @@
 import './style.css';
 import './scripts/post';
-import type { Post } from './scripts/types';
-import { renderPosts, savePost } from './scripts/post';
-import { v4 as generateId } from 'uuid';
+import './scripts/form';
+import { renderPosts } from './scripts/post';
 
 renderPosts();
-
-const form = document.querySelector<HTMLFormElement>('[data-add-post-form]');
-const inputTitle =
-	document.querySelector<HTMLInputElement>('[data-input-title]');
-const inputContent = document.querySelector<HTMLTextAreaElement>(
-	'[data-input-content]'
-);
-const inputAuthor = document.querySelector<HTMLTextAreaElement>(
-	'[data-input-author]'
-);
-
-form?.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	const today = new Date();
-	const year = today.getFullYear();
-	const month = today.getMonth() + 1;
-	const date = today.getDate();
-	const createdAt = `${year}-${month}-${date}`;
-
-	const newPost: Post = {
-		id: generateId(),
-		title: inputTitle?.value ?? '',
-		content: inputContent?.value ?? '',
-		author: inputAuthor?.value ?? '',
-		createdAt: createdAt,
-	};
-
-	savePost(newPost);
-	location.reload();
-});
---
+++ bloggporten/src/scripts/form.ts
@@ -0,0 +1,34 @@
+import type { Post } from './types';
+import { savePost } from './post';
+import { v4 as generateId } from 'uuid';
+
+const form = document.querySelector<HTMLFormElement>('[data-add-post-form]');
+const inputTitle =
+	document.querySelector<HTMLInputElement>('[data-input-title]');
+const inputContent = document.querySelector<HTMLTextAreaElement>(
+	'[data-input-content]'
+);
+const inputAuthor = document.querySelector<HTMLTextAreaElement>(
+	'[data-input-author]'
+);
+
+form?.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const today = new Date();
+	const year = today.getFullYear();
+	const month = today.getMonth() + 1;
+	const date = today.getDate();
+	const createdAt = `${year}-${month}-${date}`;
+
+	const newPost: Post = {
+		id: generateId(),
+		title: inputTitle?.value ?? '',
+		content: inputContent?.value ?? '',
+		author: inputAuthor?.value ?? '',
+		createdAt: createdAt,
+	};
+
+	savePost(newPost);
+	location.reload();
+});
